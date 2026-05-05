303f86f0687f8ecf3c17dba9b94fa620ae974033
Davi Berbek <daviberbek@gmail.com>
2026-05-05 15:31:27 -0300

    feat

---
+++ script.js
@@ -0,0 +1,17 @@
+var tl= gsap.timeline({
+    scrollTrigger:{
+        trigger: ".two",
+        start: "0% 95%",
+        end:"70% 50%",
+        scrub: true,
+        markers: true,
+    },
+});
+
+
+tl.to(
+    "#fanta",
+    {
+        top:"120%"
+    }
+)
